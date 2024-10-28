d19a1abb15fa5fa1a8688a2086e8e96db9d034b7
Lilian Kasem <likasem@microsoft.com>
2024-10-28 10:33:43 -0700

    Add tests

--- test/WebJobs.Script.Tests/Eventing/SystemLoggerProviderTests.cs
+++ test/WebJobs.Script.Tests/Eventing/SystemLoggerProviderTests.cs
@@ -3,6 +3,7 @@
 
 using System.IO;
 using Microsoft.Azure.WebJobs.Logging;
+using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.Configuration;
 using Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics;
 using Microsoft.Extensions.Logging;
@@ -28,12 +29,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             };
 
             _options = new OptionsWrapper<ScriptJobHostOptions>(scriptOptions);
+            _environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebJobsFeatureFlags, ScriptConstants.FeatureFlagEnableHostLogs);
 
             var debugStateProvider = new Mock<IDebugStateProvider>(MockBehavior.Strict);
             debugStateProvider.Setup(p => p.InDiagnosticMode).Returns(() => _inDiagnosticMode);
 
             var appServiceOptions = new TestOptionsMonitor<AppServiceOptions>(new AppServiceOptions());
-            _provider = new SystemLoggerProvider(_options, null, _environment, debugStateProvider.Object, null, appServiceOptions);
+            var hostingOptions = new TestOptionsMonitor<FunctionsHostingConfigOptions>(new FunctionsHostingConfigOptions());
+            _provider = new SystemLoggerProvider(_options, null, _environment, debugStateProvider.Object, null, appServiceOptions, hostingOptions);
         }
 
         [Fact]
@@ -41,7 +44,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         {
             Assert.IsType<SystemLogger>(_provider.CreateLogger(LogCategories.CreateFunctionCategory("TestFunction")));
             Assert.IsType<SystemLogger>(_provider.CreateLogger(ScriptConstants.LogCategoryHostGeneral));
-            Assert.IsType<SystemLogger>(_provider.CreateLogger("NotAFunction.TestFunction.User"));
+           //Assert.IsType<SystemLogger>(_provider.CreateLogger("NotAFunction.TestFunction.User"));
         }
 
         [Fact]
@@ -81,6 +84,45 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             Assert.True(logger.IsEnabled(LogLevel.Critical));
             Assert.True(logger.IsEnabled(LogLevel.Debug));
             Assert.True(logger.IsEnabled(LogLevel.Trace));
+        }
+
+        [Theory]
+        [InlineData(true, false, true)] // RestrictHostLogs is true, FeatureFlag is not set, should result in **restricted** logs. This is the default behaviour of the host.
+        [InlineData(false, true, false)] // RestrictHostLogs is false, FeatureFlag is set, should result in unrestricted logs
+        [InlineData(true, true, false)] // RestrictHostLogs is true, FeatureFlag is set, should result in unrestricted logs
+        [InlineData(false, false, false)] // RestrictHostLogs is false, FeatureFlag is not set, should result in unrestricted log
+        public void RestirctHostLogs_SetsCorrectSystemLogPrefix(bool restrictHostLogs, bool setFeatureFlag, bool shouldResultInRestrictedSystemLogs)
+        {
+            TestEnvironment environment = new ();
+            var debugStateProvider = new Mock<IDebugStateProvider>(MockBehavior.Strict);
+            debugStateProvider.Setup(p => p.InDiagnosticMode).Returns(() => false);
+
+            var hostingOptions = new FunctionsHostingConfigOptions();
+            hostingOptions.RestrictHostLogs = restrictHostLogs;
+            var hostingOptionsMonitor = new TestOptionsMonitor<FunctionsHostingConfigOptions>(hostingOptions);
+
+            if (setFeatureFlag)
+            {
+                environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebJobsFeatureFlags, ScriptConstants.FeatureFlagEnableHostLogs);
+            }
+
+            var appServiceOptions = new TestOptionsMonitor<AppServiceOptions>(new AppServiceOptions());
+            var provider = new SystemLoggerProvider(_options, null, environment, debugStateProvider.Object, null, appServiceOptions, hostingOptionsMonitor);
+
+            var logger = provider.CreateLogger(LogCategories.HostGeneral);
+            //LogCategories.Startup
+
+            Assert.Equal(restrictHostLogs, hostingOptionsMonitor.CurrentValue.RestrictHostLogs);
+            Assert.Equal(setFeatureFlag, FeatureFlags.IsEnabled(ScriptConstants.FeatureFlagEnableHostLogs, environment));
+
+            if (shouldResultInRestrictedSystemLogs)
+            {
+                Assert.IsType<NullLogger>(logger);
+            }
+            else
+            {
+                Assert.IsType<SystemLogger>(logger);
+            }
         }
     }
 }
