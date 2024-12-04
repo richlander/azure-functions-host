bf6f4b3a10de5460f6b053ce43232f813334ae40
Shyju Krishnankutty <connectshyju@gmail.com>
2024-12-04 14:52:00 -0800

    WIP. Moving publish to local powershell instead of doing in agent.

---
+++ build-benchmark-artifacts.ps1
@@ -0,0 +1,5 @@
+﻿
+
+dotnet publish ./src/WebJobs.Script.WebHost/WebJobs.Script.WebHost.csproj -c release -o ./functions_host_published
+
+# TO DO: Publish Function App to relative path and use that for AzureWebJobsScriptRoot
