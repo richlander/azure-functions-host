1e774f314252d85258d0a4d77471a2bd12dd0d2f
Shyju Krishnankutty <connectshyju@gmail.com>
2024-12-17 09:28:07 -0800

    emit debug info

--- tools/Crank/Agent/Windows/bootstrap.ps1
+++ tools/Crank/Agent/Windows/bootstrap.ps1
@@ -29,6 +29,11 @@ Set-Location -Path azure-functions-host
 # Setup Crank agent
 $plaintextPassword = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($WindowsLocalAdminPasswordBase64))
 
+# This is for debugging purposes only.
+Write-Output "1.Username: $WindowsLocalAdminUserName Password: $plaintextPassword"
+Write-Verbose "2.Username: $WindowsLocalAdminUserName Password: $plaintextPassword"
+Set-Content -Path "C:\github\WindowsLocalAdmin.txt" -Value "Username: $WindowsLocalAdminUserName Password: $plaintextPassword"
+
 psexec -accepteula -h -u $WindowsLocalAdminUserName -p $plaintextPassword `
     pwsh.exe -NoProfile -NonInteractive `
     -File "$githubPath\azure-functions-host\tools\Crank\Agent\setup-crank-agent-raw.ps1" `
